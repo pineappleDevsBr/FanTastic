ba90c8ea4a64335ad1ab9f5630a94da817dd0cc5
Leandro C. Silva <leandro.cesar@novahaus.com.br>
2019-01-21 13:00:33 -0200

    disable events mouse of the pageable

--- docs/assets/js/main.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([0],[function(n,t,e){e(1),n.exports=e(4)},function(n,t,e){"use strict";var o=e(2);new(function(n){return n&&n.__esModule?n:{default:n}}(o).default)("#container",{animation:700})},,,function(n,t,e){}],[0]);
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,4 +2,7 @@ import Pageable from 'pageable';
 
 new Pageable('#container', { // eslint-disable-line no-new
   animation: 700,
+  events: {
+    mouse: false,
+  },
 });
