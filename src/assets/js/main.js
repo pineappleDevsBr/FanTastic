a45d9890b1e8917541ac8527cf9713e766c48a04
Leandro C. Silva <leandrocesar49@hotmail.com>
2019-04-10 21:49:30 -0300

    Remove switch tab, add simple-tabs-js, and start probability-uniform

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,11 +1,11 @@
-import SwitchTab from './components/switchTab';
+import SimpleTabs from 'simple-tabs-js';
 import JumpButton from './components/jumpButton';
 import AccessibillityBar from './components/accessibility-bar';
 import HelpBox from './components/helpBox';
 import Descriptive from './components/descriptive';
 
-SwitchTab.create('[data-form]');
-SwitchTab.create('[data-dropdown]');
+const tabs = new SimpleTabs('[data-form]'); // eslint-disable-line
+
 JumpButton.create('[data-button-home]', '.s-section--form');
 HelpBox.create('[data-help-box]');
 AccessibillityBar.create('[data-accessibillity-bar]');
