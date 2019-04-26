0025bf56ec7254d1ba9850d484aa5164c38409f6
Leandro C. Silva <leandro.cesar@novahaus.com.br>
2019-04-26 19:13:16 -0300

    Add calc's of binomial && add math.js

--- src/assets/js/components/binomial.js
+++ src/assets/js/components/binomial.js
@@ -1,4 +1,5 @@
 import Modal from 'micromodal';
+import * as math from 'mathjs';
 
 class Binomial {
   constructor() {
@@ -9,6 +10,7 @@ class Binomial {
     this.event = null;
     this.sucess = null;
     this.failure = null;
+    this.probabilityValue = [];
 
     this.setup();
   }
@@ -29,12 +31,13 @@ class Binomial {
   recoverData() {
     const bigData = document.querySelector('[data-binomial]');
 
-    this.size = Number(bigData.querySelector('[data-size]').value);
-    this.sucess = Number(bigData.querySelector('[data-sucess]').value);
-    this.failure = Number(bigData.querySelector('[data-failure]').value);
+    this.size = parseFloat(bigData.querySelector('[data-size]').value);
+    this.sucess = parseFloat(bigData.querySelector('[data-sucess]').value);
+    this.failure = parseFloat(bigData.querySelector('[data-failure]').value);
 
     this.event = (bigData.querySelector('[data-event]').value).split(/;/);
-    this.event = this.event.map(num => Number(num));
+    this.event = this.event.map(num => parseFloat(num));
+    console.log(this.event);
   }
 
   validateData() {
@@ -42,9 +45,28 @@ class Binomial {
       this.modalMessage.innerHTML = 'Preencha todos os dados!';
       Modal.show('modal-1');
     } else {
-      this.modalMessage.innerHTML = `size: ${this.size}<br />event: ${this.event}<br />sucess: ${this.sucess}<br />failure ${this.failure}`; // eslint-disable-line
-      Modal.show('modal-1');
+      this.probability();
+      // this.median();
+      // this.standardDeviation();
     }
+  }
+
+  probability() {
+    this.event.forEach((i) => {
+      const pao = {
+        a: Binomial.combinatorialAnalysis(this.size, this.event[i]),
+        b: Math.pow(this.sucess, this.event[i]), // eslint-disable-line
+        c: Math.pow(this.failure, (this.size - this.event[i])) // eslint-disable-line
+      };
+
+      console.log(pao);
+    });
+
+    console.log(this.probabilityValue);
+  }
+
+  static combinatorialAnalysis(n, k) {
+    return (math.factorial(n) / ((math.factorial(k) * (math.factorial(n - k)))));
   }
 }
 
