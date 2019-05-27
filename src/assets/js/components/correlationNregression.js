52fa579ea3b6a6cefcb0c7411c9cfabf181b89cc
Leandro C. Silva <leandrocesar49@hotmail.com>
2019-05-27 09:22:08 -0300

    Resolve bugs && organize Code

--- src/assets/js/components/correlationNregression.js
+++ src/assets/js/components/correlationNregression.js
@@ -1,20 +1,28 @@
 import MicroModal from 'micromodal';
+import Jump from 'jump.js';
+import doT from 'dot';
 
 class CorrelationNRegression {
   constructor() {
     this.submitButton = document.querySelector('[data-button-cr]');
     this.modalMessage = document.querySelector('[data-modal]').querySelector('[data-modal-message]');
+    this.holderResult = document.querySelector('[data-result-holder]');
+    this.canvasHolder = document.querySelector('[data-canvas]');
+    this.containerResult = document.createElement('div');
+    this.crTemplate = doT.template('<div class="c-prob-result"><p class="c-prob-result__cell"><b>Coeficiente de correlação:</b><br /> {{=it.cr}}</p><p class="c-prob-result__cell"><b>Nivel de correlção:</b> <br />{{=it.crNvl}}</p><p class="c-prob-result__cell"><b>Equação da reta:</b> <br />{{=it.equaCR}}</p></div>');
     this.dataX = { name: null, value: null, somX: null, somXsqr: null };
     this.dataY = { name: null, value: null, somY: null, somYsqr: null };
     this.dataXY = null;
     this.dataR = null;
     this.dataA = null;
     this.dataB = null;
+    this.rateR = null;
     this.inputX = document.createElement('input');
     this.inputY = document.createElement('input');
-    this.container = document.createElement('div');
+    this.containerFuture = document.createElement('div');
     this.valueX = null;
     this.valueY = null;
+    this.result = null;
     this.setup();
   }
 
@@ -47,6 +55,8 @@ class CorrelationNRegression {
       this.generateR();
       this.regression();
       this.futureProjection();
+      this.createResult();
+      this.appendResult();
     }
   }
 
@@ -58,6 +68,12 @@ class CorrelationNRegression {
   }
 
   generateMatrix() {
+    this.dataX.somX = null;
+    this.dataY.somY = null;
+    this.dataX.somXsqr = null;
+    this.dataY.somYsqr = null;
+    this.dataXY = null;
+
     for (let index = 0; index < this.dataX.value.length; index += 1) {
       this.dataX.somX += this.dataX.value[index];
       this.dataY.somY += this.dataY.value[index];
@@ -75,6 +91,17 @@ class CorrelationNRegression {
     const qd2 = (size * this.dataY.somYsqr) - Math.pow(this.dataY.somY, 2); // eslint-disable-line
 
     this.dataR = this.dataR / Math.sqrt(qd1 * qd2);
+    this.dataR = this.dataR * 100;
+    this.dataR = Math.round(this.dataR);
+    this.dataR = this.dataR / 100;
+
+    if (this.dataR < 0.3) {
+      this.rateR = 'inexistente ou muito fraca';
+    } else if (this.dataR >= 0.3 && this.dataR < 0.6) {
+      this.rateR = 'fraca';
+    } else if (this.dataR >= 0.6 && this.dataR <= 1) {
+      this.rateR = 'média a forte';
+    }
   }
 
   regression() {
@@ -87,23 +114,22 @@ class CorrelationNRegression {
   }
 
   futureProjection() {
+    this.containerFuture.innerHTML = '';
     this.inputX.addEventListener('keyup', (e) => { this.listenerX(e); });
     this.inputY.addEventListener('keyup', (e) => { this.listenerY(e); });
 
-    const testeA = document.createElement('p');
-    const textoA = document.createTextNode(` = ${this.dataA}`);
-    const testeB = document.createElement('p');
-    const textoB = document.createTextNode(`+${this.dataB}`);
+    const elemA = document.createElement('p');
+    const nodeA = document.createTextNode(` = ${this.dataA} x`);
+    const elemB = document.createElement('p');
+    const nodeB = document.createTextNode(`+${this.dataB}`);
 
-    testeA.appendChild(textoA);
-    testeB.appendChild(textoB);
+    elemA.appendChild(nodeA);
+    elemB.appendChild(nodeB);
 
-    this.container.appendChild(this.inputY);
-    this.container.appendChild(testeA);
-    this.container.appendChild(this.inputX);
-    this.container.appendChild(testeB);
-
-    document.body.appendChild(this.container);
+    this.containerFuture.appendChild(this.inputY);
+    this.containerFuture.appendChild(elemA);
+    this.containerFuture.appendChild(this.inputX);
+    this.containerFuture.appendChild(elemB);
   }
 
   listenerX(e) {
@@ -114,7 +140,6 @@ class CorrelationNRegression {
 
       this.valueY = ((this.dataA * this.valueX) + this.dataB).toFixed(2);
       this.inputY.value = this.valueY;
-      console.log(this.valueX); //eslint-disable-line
     }
   }
 
@@ -125,7 +150,37 @@ class CorrelationNRegression {
       this.valueY = e.currentTarget.value;
       this.valueX = ((this.valueY - this.dataB) / this.dataA).toFixed(2);
       this.inputX.value = this.valueX;
-      console.log(this.valueY); //eslint-disable-line
+    }
+  }
+
+  createResult() {
+    this.containerResult.innerHTML = '';
+    this.result = this.crTemplate({
+      cr: this.dataR,
+      crNvl: this.rateR,
+      equaCR: `y = ${this.dataA}x + ${this.dataB}`,
+    });
+
+    const resultCells = document.createElement('div');
+
+    resultCells.innerHTML = this.result;
+
+    this.containerResult.appendChild(resultCells);
+    this.containerResult.appendChild(this.containerFuture);
+  }
+
+  appendResult() {
+    if (this.containerResult !== undefined) {
+      if (this.holderResult.className.indexOf('is-active') === -1) {
+        this.holderResult.classList.add('is-active');
+      }
+
+      this.holderResult.querySelector('[data-table-result]').innerHTML = '';
+      this.canvasHolder.innerHTML = '';
+      this.holderResult.querySelector('[data-table-result]').appendChild(this.containerResult);
+      setTimeout(() => {
+        Jump('.s-section--result');
+      }, 500);
     }
   }
 }
